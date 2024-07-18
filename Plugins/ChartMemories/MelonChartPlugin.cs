e055be2ec41fee123df1c2ba707719e8d5b166cd
Justin Yoo <justin.yoojh@gmail.com>
2024-07-18 15:26:34 +0900

    Update prompts

--- Plugins/ChartMemories/MelonChartPlugin.cs
+++ Plugins/ChartMemories/MelonChartPlugin.cs
@@ -10,7 +10,7 @@ using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 using Microsoft.SemanticKernel.Memory;
 
-// namespace MySentimentalPlaylist.Notebooks.Plugins.MelonChartMemory;
+// namespace MySentimentalPlaylist.Notebooks.Plugins.ChartMemories;
 
 #pragma warning disable SKEXP0001
 
@@ -19,16 +19,17 @@ public class MelonChartPlugin
     private const string COLLECTION = "MelonChart";
 
     [KernelFunction, Description("Add Melon Chart data to the memory")]
-    public static async Task AddMelonChart(
+    public static async Task AddChart(
         [Description("The Semantic Memory instance")] ISemanticTextMemory memory,
         [Description("The HttpClient instance")] HttpClient http,
         [Description("The JsonSerializerOptions instance")] JsonSerializerOptions jso)
     {
         var today = DateTimeOffset.UtcNow
-                                .ToOffset(TimeZoneInfo.FindSystemTimeZoneById("Korea Standard Time").BaseUtcOffset)
+                                .ToOffset(TimeZoneInfo.FindSystemTimeZoneById("Asia/Seoul").BaseUtcOffset)
                                 .ToString("yyyyMMdd");
+        var requestUri = $"https://raw.githubusercontent.com/aliencube/MelonChart.NET/main/data/top100-{today}.json";
 
-        var data = await http.GetStringAsync($"https://raw.githubusercontent.com/aliencube/MelonChart.NET/main/data/top00-{today}.json");
+        var data = await http.GetStringAsync(requestUri);
         var chart = JsonSerializer.Deserialize<ChartItemCollection>(data, jso);
 
         foreach (var item in chart.Items)
@@ -41,30 +42,7 @@ public class MelonChartPlugin
         }
     }
 
-    [KernelFunction, Description("Add Spotify Chart data to the memory")]
-    public static async Task AddSpotifyChart(
-        [Description("The Semantic Memory instance")] ISemanticTextMemory memory,
-        [Description("The HttpClient instance")] HttpClient http,
-        [Description("The JsonSerializerOptions instance")] JsonSerializerOptions jso)
-    {
-        var today = DateTimeOffset.UtcNow
-                                .ToOffset(TimeZoneInfo.FindSystemTimeZoneById("Korea Standard Time").BaseUtcOffset)
-                                .ToString("yyyyMMdd");
-
-        var data = await http.GetStringAsync($"https://raw.githubusercontent.com/aliencube/MelonChart.NET/main/data/spotify00-{today}.json");
-        var chart = JsonSerializer.Deserialize<ChartItemCollection>(data, jso);
-
-        foreach (var item in chart.Items)
-        {
-            var index = chart.Items.IndexOf(item) + 1;
-            var serialised = JsonSerializer.Serialize(item, jso);
-            await memory.SaveInformationAsync(collection: COLLECTION, id: $"{today}-{index.ToString("000")}", text: serialised);
-
-            Console.WriteLine($"- Stored: {item.Artist} - {item.Title}");
-        }
-    }
-
-    [KernelFunction, Description("Search question from the memory")]
+    [KernelFunction, Description("Search songs from the memory")]
     public static async Task<List<ChartItem>> FindSongs(
         [Description("The Semantic Memory instance")] ISemanticTextMemory memory,
         [Description("The question")] string question,
